53dd7e7618094d2fc76068ffa146b46e4eeb6c39
Alex Mills <alexanderwmills@gmail.com>
2020-08-23 22:09:54 +1000

    Tracklist shifting and audio play both implemented

--- src/app/model/playlist.ts
+++ src/app/model/playlist.ts
@@ -12,16 +12,59 @@ export interface SpotifyPlaylist {
   images: object[];
 }
 
-export interface Track {
-  id: string;
-  basic: object;
-  advanced: object;
-}
-
 export interface Owner {
   id: string;
   display_name: string;
   uri: string;
   href: string;
   type: string;
+}
+
+export interface Track {
+  id: string;
+  basic: TrackBasicObject;
+  advanced: TrackAdvancedObject;
+}
+
+export interface TrackBasicObject {
+  id: string;
+  name: string;
+  duration_ms: number;
+  popularity: number;
+  preview_url: string;
+  uri: string;
+  href: string;
+  artists: Artist[];
+  album: Album;
+}
+
+export interface TrackAdvancedObject {
+  id: string;
+  acousticness: number;
+  danceability: number;
+  tempo: number;
+  valence: number;
+  speechiness: number;
+  liveness: number;
+  time_signature: number;
+  loudness: number;
+  key: number;
+  energy: number;
+  analysis_url: string;
+  instrumentalness: number;
+}
+
+export interface Artist {
+  id: string;
+  name: string;
+  uri: string;
+  href: string;
+}
+
+export interface Album {
+  id: string;
+  name: string;
+  href: string;
+  uri: string;
+  release_date: string;
 }
--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -13,7 +13,6 @@ import { AuthoriseService } from '../services/authorise.service';
 })
 export class TracksComponent implements OnInit {
 
-  selectedTrack: Track;
   @Input() tracks: Track[];
 
   constructor(private trackService: TrackService, private messageService: MessageService, private authoriseService: AuthoriseService) { }
@@ -23,24 +22,40 @@ export class TracksComponent implements OnInit {
     // this.getTracks();
   }
 
-  // getTracks() {
-  //   console.log("Access Token: "+this.authoriseService.access_token);
-  //   if (this.authoriseService.access_token != "No token") {
-  //     this.trackService.getTracks("1qdcXSvXuSAjt4GQjir7iZ").subscribe(tracks => {
-  //       // Once aync operation completes...
-  //       this.tracks = tracks;
-  //       console.log(this.tracks);
-  //       console.log(this.tracks[0]);
-  //     });
-  //   }
-  // }
+  playSample(trackid: string): void {
+    var audio = <HTMLAudioElement>document.getElementById(trackid);
+    if(audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
 
   moveUp(track: Track): void {
-    // console.log("Move "+track.basic.name+" up");
+    console.log("Moving "+track.basic.name+" up");
+    console.log(track.basic);
+    // Starting at index 1, because you can't move the top track up
+    for (var i = 1; i < this.tracks.length; i++) {
+      if (this.tracks[i].id == track.id) {
+        // Because we have this track stored locally, we move the secondary track first
+        this.tracks.splice(i,1,this.tracks[i-1]);
+        this.tracks.splice(i-1,1,track);
+      }
+    }
   }
 
   moveDown(track: Track): void {
-    // console.log("Move "+track.basic.name+" down");
+    console.log("Moving "+track.basic.name+" down");
+    console.log(track.advanced);
+    // Ending at length - 1, because you can't move the bottom track down
+    for (var i = 0; i < this.tracks.length - 1; i++) {
+      if (this.tracks[i].id == track.id) {
+        this.tracks.splice(i,1,this.tracks[i+1]);
+        this.tracks.splice(i+1,1,track);
+        // Return to avoid it meeting criteria on next loop and being forced down to bottom
+        return;
+      }
+    }
   }
 
 }
